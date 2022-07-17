d529eecec7853690b7e80e3db429ad6fd07f63b0
Andrew Gerst <gerst20051@gmail.com>
2022-07-17 04:38:04 -0700

    simplify hello world app and update eslint config

--- MyTestApp/App.js
+++ MyTestApp/App.js
@@ -1,17 +1,16 @@
 import React from 'react';
-import type {Node} from 'react';
-import {SafeAreaView, ScrollView, StatusBar, Text, View} from 'react-native';
+import { Text, View } from 'react-native';
 
-const App: () => Node = () => {
+const App = () => {
   return (
-    <SafeAreaView>
-      <StatusBar />
-      <ScrollView>
-        <View>
-          <Text>Hello World</Text>
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <View
+      style={{
+        alignItems: 'center',
+        flex: 1,
+        justifyContent: 'center',
+      }}>
+      <Text>Hello, world!</Text>
+    </View>
   );
 };
 
